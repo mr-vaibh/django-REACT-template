c0b306967e2c7086dbbeb243bf0c43db5b71a3e9
Vaibhav Shukla <52421094+mr-vaibh@users.noreply.github.com>
2021-04-03 13:12:14 +0530

    Update and rename Content.js to Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function Content() {
+export default function Home() {
     return (
         <div className="container">
             <div className="starter-template">
